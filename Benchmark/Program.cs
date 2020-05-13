8eae19726734f0a7b8e5a2b4a59ea0297e68addb
sonnemaf <fons.sonnemans@reflectionit.nl>
2020-05-13 14:45:52 +0200

    Add project files.

---
+++ Benchmark/Program.cs
@@ -0,0 +1,61 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using System;
+
+namespace Benchmark {
+
+    class Program {
+
+        static void Main(string[] args) {
+            // Install-Package BenchmarkDotNet 
+            BenchmarkRunner.Run<DateTimeBM>();
+
+            //BenchmarkRunner.Run<PointBM>();
+        }
+    }
+
+    [SimpleJob(RuntimeMoniker.Net48)]
+    [SimpleJob(RuntimeMoniker.NetCoreApp31, baseline: true)]
+    public class DateTimeBM {
+
+        private DateTime _date1;
+        private readonly DateTime _date2;
+
+        [Benchmark] public int NotReadonly() => _date1.Year;
+
+        [Benchmark] public int Readonly() => _date2.Year;
+    }
+
+
+    public class PointBM {
+
+        private readonly Point3D _p1;
+
+        [Benchmark]
+        public void NotReadonly() {
+            for (int i = 0; i < 100; i++) {
+                _p1.Foo();
+            }
+        }
+
+        [Benchmark(Baseline = true)]
+        public void Readonly() {
+            for (int i = 0; i < 100; i++) {
+                _p1.Bar();
+            }
+        }
+    }
+
+
+
+    struct Point3D {
+
+        public double X;
+        public double Y;
+        public double Z;
+
+        public double Foo() => X + 1;
+        public readonly double Bar() => X + 1;
+    }
+}
---
+++ DefensiveCopies/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace StructVersusClass {
+
+    class Program {
+
+        private static Point _p1 = new Point(3, 4);
+        private static readonly Point _p2 = new Point(3, 4);
+
+        static void Main(string[] args) {
+            Console.WriteLine(_p1.ToString()); // (3,4)
+            Console.WriteLine(_p1.Distance);   // 5
+            Console.WriteLine();
+
+            _p2.Swap(); // Defensive Copy, the copy is swapped
+            Console.WriteLine(_p2.ToString()); // (3,4)
+            Console.WriteLine(_p2.Distance);   // 5
+            Console.WriteLine();
+
+            Point p3 = _p1; // Value copy
+
+            p3.Swap();
+            //p3 = p3.Swap();
+
+            Console.WriteLine(p3.ToString());  // (4,3)
+            Console.WriteLine(p3.Distance);    // 5
+            Console.WriteLine();
+
+            ref readonly Point p4 = ref _p1;
+            p4.Swap(); // Defensive Copy, the copy is swapped
+            Console.WriteLine(p4.ToString()); // (3,4)
+            Console.WriteLine(p4.Distance);   // 5
+            Console.WriteLine();
+
+            Foo(_p1); // (3,4) & 5
+        }
+
+        static void Foo(in Point p) {
+            p.Swap(); // Defensive Copy, the copy is swapped
+            Console.WriteLine(p.ToString());
+            Console.WriteLine(p.Distance);
+        }
+    }
+
+
+    struct Point {
+
+        public double X, Y;
+
+        public readonly void Foo() {
+            X = 12;
+            Swap();
+        }
+
+        public void Swap() => this = new Point(Y, X);
+
+        //public readonly Point Swap() => new Point(Y, X);
+
+        public Point(double x, double y) { X = x; Y = y; }
+
+
+        public readonly double Distance => Math.Sqrt((X * X) + (Y * Y));
+
+        public readonly override string ToString() => $"({X.ToString()},{Y.ToString()})";
+
+
+    }
+
+}
